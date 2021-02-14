306375a90b384eb9a4ee9fec730b9dcebb8505bf
Kamaljeet <50160354+KamaljeetSahoo@users.noreply.github.com>
2021-02-14 09:47:27 +0530

    redirects and single post updated

--- src/components/Post.js
+++ src/components/Post.js
@@ -31,7 +31,7 @@ function Post() {
                     <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
                     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                         {postData && postData.map((post, index) => (
-                        <article>
+                        <article key={index}>
                             <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                                 <span className="block h-64 relative rounded shadow leading-snug bg-white border-green-400" key={index}>
                                     <img 
@@ -39,8 +39,8 @@ function Post() {
                                         alt={post.mainImage.alt}
                                         className="w-full h-full rounded-r object-cover absolute"
                                     />
-                                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                        <h3 className="text-lg text-gray-800 font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">{post.title}</h3>
+                                    <span className="block relative h-full flex justify-end items-end pr-4 pb-4" key={index}>
+                                        <h3 className="text-lg text-gray-800 font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded" key={index}>{post.title}</h3>
                                     </span>
                                 </span>
                             </Link>
